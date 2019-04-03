8fce177ffcc263a85aad6849f60fe15e3ac74551
Amer <amerdtx@gmail.com>
2019-04-02 19:38:14 -0500

    changes in node js file

--- spotifyRequest.js
+++ spotifyRequest.js
@@ -1,52 +1,53 @@
+//Commands Run
+//curl command info web url- https://developer.spotify.com/console/get-current-user-top-artists-and-tracks/?type=artists&time_range=short_term&limit=10&offset=
+
+//step 1: run node js command as node (fileaname) in command prompt
+//step 2: get the curl command based on the link above
+//step 3:
+
 var SpotifyWebApi = require('spotify-web-api-node');
 
 
+
 var clientId = 'f4c03ff11fe8453b8654e0ed3bbd06d2',
-  clientSecret = '882213cd34b24c99b9c0a10da2128294';
+ clientSecret = '6d58b8cc1f6d4d1ab70ebb7cf2eb5c2b';
 
-// Create the api object with the credentials
-var spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret
-});
+// // Create the api object with the credentials
+// var spotifyApi = new SpotifyWebApi({
+//   clientId: clientId,
+//   clientSecret: clientSecret
+// });
 
-// Retrieve an access token.
+var spotifyApi = new SpotifyWebApi();
+
+// //get an artists top tracks
+// spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'GB')
+//   .then(function(data) {
+//     console.log(data.body);
+//     }, function(err) {
+//     console.log('Something went wrong!', err);
+//   });
+
+Retrieve an access token.
 spotifyApi.clientCredentialsGrant().then(
   function(data) {
     console.log('The access token expires in ' + data.body['expires_in']);
     console.log('The access token is ' + data.body['access_token']);
 
-
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token']);
-  },
 
+  },
 
   function(err) {
     console.log('Something went wrong when retrieving an access token', err);
   }
 );
 
-//retrieve new releases
-spotifyApi.getNewReleases({ limit : 5, offset: 0, country: 'USA' })
+// get an artist id
+spotifyApi.searchArtists('young the giant')
   .then(function(data) {
-    console.log(data.body);
-      done();
-    }, function(err) {
-       console.log("Something went wrong!", err);
-    });
+    console.log('Search artists by "young the giant"', data.body);
+  }, function(err) {
+    console.error(err);
   });
-
-
-//.ajax command
-$.ajax("https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5",{
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer BQDha8GGRZTQxUgSbxQHrew5ekOysffEQ0Y_OXtnc27ibTQS7lyQWuQFHjDlROhLvnWC5Y29moXonFd6C1yGKnZSnGBEDYHiwJY_joKZDtrZti95Y6IF0zUutFuXgbi6wVnNxqR5rXLR2m6e6qLjDyBN"
-  }
-
-},
-  success: function (data) {
-         console.log(data);
-   }
-)
